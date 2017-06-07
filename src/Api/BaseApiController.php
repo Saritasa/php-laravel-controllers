92ac165a1af8730c38a0084963f1957966c07f3a
Sergey Populov <sergey@saritasa.com>
2017-06-07 12:32:38 +0500

    Remove duplicates of BaseApiController

--- src/Api/BaseApiController.php
+++ src/Api/BaseApiController.php
@@ -2,13 +2,16 @@
 
 namespace Saritasa\Laravel\Controllers\Api;
 
-use Saritasa\Transformers\BaseTransformer;
 use App\Models\User;
+use Dingo\Api\Http\Request;
 use Dingo\Api\Http\Response;
 use Dingo\Api\Routing\Helpers;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
+use Illuminate\Contracts\Validation\Validator;
+use Saritasa\DingoApi\Exceptions\ValidationException;
+use Saritasa\Transformers\BaseTransformer;
 use Saritasa\Transformers\IDataTransformer;
 
 /**
@@ -37,5 +40,15 @@ abstract class BaseApiController extends Controller
             return $this->response->paginator($data, $t);
         }
         return $this->response->item($data, $t);
+    }
+
+    public function validate(Request $request, array $rules, array $messages = [], array $customAttributes = [])
+    {
+        /** @var Validator $validator */
+        $validator = $this->getValidationFactory()->make($request->all(), $rules, $messages, $customAttributes);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator->errors());
+        }
     }
 }
