50b2ded81175c2c73cfca505c5bdbe5036ce9d39
Evgeny Mosunov <eugene.mosunov@saritasa.com>
2018-04-04 16:24:07 +0500

    Improve JWTAuthApiController

--- src/Api/JWTAuthApiController.php
+++ src/Api/JWTAuthApiController.php
@@ -5,29 +5,49 @@ namespace Saritasa\Laravel\Controllers\Api;
 use Dingo\Api\Http\Response;
 use Saritasa\Laravel\Controllers\Requests\LoginRequest;
 use Saritasa\Laravel\Controllers\Responses\AuthSuccess;
+use Saritasa\Transformers\IDataTransformer;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Tymon\JWTAuth\Exceptions\JWTException;
-use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\JWTAuth;
 
 /**
- * Authenticate API Controller. Uses JWT authentication
+ * Authenticate API Controller. Uses JWT authentication.
  */
 class JWTAuthApiController extends BaseApiController
 {
+    /**
+     * Jwt auth service.
+     *
+     * @var JWTAuth
+     */
+    protected $jwtAuth;
 
     /**
-     * Authenticate user
+     * Authenticate API Controller. Uses JWT authentication.
+     *
+     * @param JWTAuth $jwtAuth Jwt auth service
+     * @param IDataTransformer $transformer default transformer to apply to handled entity.
+     */
+    public function __construct(JWTAuth $jwtAuth, IDataTransformer $transformer = null)
+    {
+        $this->jwtAuth = $jwtAuth;
+        parent::__construct($transformer);
+    }
+
+    /**
      * Authenticate user by `email` and `password`.
      *
      * @param LoginRequest $request HTTP Request
+     *
      * @return Response
      *
-     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     * @throws HttpException
      */
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request): Response
     {
         $credentials = $request->only('email', 'password');
         try {
-            if (!$token = JWTAuth::attempt($credentials)) {
+            if (!$token = $this->jwtAuth->attempt($credentials)) {
                 $this->response->errorNotFound(trans('controllers::auth.failed'));
             }
         } catch (JWTException $e) {
@@ -38,27 +58,28 @@ class JWTAuthApiController extends BaseApiController
     }
 
     /**
-     * Logout user
+     * Logout user.
+     * Invalidate access token.
      *
-     * Invalidate access token
+     * @return Response
      */
-    public function logout()
+    public function logout(): Response
     {
-        JWTAuth::invalidate(JWTAuth::getToken());
+        $this->jwtAuth->invalidate();
         return $this->response->noContent();
     }
 
     /**
-     * Refresh the access token
+     * Refresh the access token.
      *
      * @return Response
-     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     *
+     * @throws HttpException
      */
-    public function refreshToken()
+    public function refreshToken(): Response
     {
         try {
-            $token = JWTAuth::getToken();
-            $newToken = JWTAuth::refresh($token);
+            $newToken = $this->jwtAuth->refresh();
             return $this->json(new AuthSuccess($newToken));
         } catch (JWTException $e) {
             $this->response->errorUnauthorized(trans('controllers::auth.jwt_refresh_error'));
