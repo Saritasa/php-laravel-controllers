24c9ef2a2462e662d50ebdf45c053187689153b4
Serge Populov <sergey@saritasa.com>
2019-07-26 13:49:21 +0500

    Update auth.php

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -2,5 +2,6 @@
 return [
     'failed' => 'These credentials do not match our records.',
     'jwt_error' => 'Could not create token',
-    'jwt_refresh_error' => 'Could not refresh token'
+    'jwt_refresh_error' => 'Could not refresh token',
+    'jwt_blacklist_error' => 'Could not blacklist token',
 ];
