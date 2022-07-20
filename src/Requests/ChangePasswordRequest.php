70480931609ab834760b604a3e0b4255e0c79c0f
Sergey Populov <sergey@saritasa.com>
2022-07-20 17:35:12 +0500

    Add PageRequest and ChangePasswordRequest classes

---
+++ src/Requests/ChangePasswordRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Saritasa\LaravelControllers\Requests;
+
+use Saritasa\Laravel\Validation\Rules\ContainsLowercase;
+use Saritasa\Laravel\Validation\Rules\ContainsNumeral;
+use Saritasa\Laravel\Validation\Rules\ContainsUppercase;
+
+/**
+ * The HTTP Request with information to change a user password.
+ *
+ * @property-read string $oldPassword The old user password
+ * @property-read string $newPassword The new password to set
+ */
+class ChangePasswordRequest extends Request
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return string[]
+     */
+    public function rules(): array
+    {
+        return [
+            'oldPassword' => ['required', 'string'],
+            'newPassword' => [
+                'required',
+                'string',
+                'min:8',
+                new ContainsNumeral(),
+                new ContainsLowercase(),
+                new ContainsUppercase(),
+            ],
+        ];
+    }
+}
---
+++ src/Requests/PageRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Saritasa\LaravelControllers\Requests;
+
+use Saritasa\DingoApi\Paging\PagingInfo;
+
+/**
+ * Contains information about page number and items per page count.
+ *
+ * @property-read int $page
+ * @property-read int $per_page
+ */
+class PageRequest extends Request
+{
+    public const PER_PAGE = 'per_page';
+    public const PAGE = 'page';
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @param string[]|array $rules The array of rules for request
+     *
+     * @return string[]
+     */
+    public function rules(array $rules = []): array
+    {
+        return array_merge([
+            self::PAGE => 'integer|min:1',
+            self::PER_PAGE => 'integer|min:1',
+        ], $rules);
+    }
+
+    /**
+     * Returns Paging Data from request.
+     *
+     * @return PagingInfo
+     */
+    public function getPageInfo(): PagingInfo
+    {
+        return new PagingInfo([
+            self::PAGE => $this->page,
+            self::PER_PAGE => $this->per_page,
+        ]);
+    }
+}
